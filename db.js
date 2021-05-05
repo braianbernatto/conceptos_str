be3da34a0241d77d2793bfc2ec2d7c84ecec3356
Bernatto <braianbernatto@hotmail.com>
2021-05-05 19:47:59 -0400

    heroku update v5

--- db.js
+++ db.js
@@ -9,12 +9,17 @@ const connectionString =
     database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
     port: process.env.PGPORT,
-    ssl: true
+    ssl: {rejectUnauthorized : false,
+    }
   }
-// `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`
-
+//`postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}?ssl=true`
 const pool = Pool({
-    connectionString: connectionString
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+    ssl: process.env.PGHOST == "localhost" ? false : true
 })
 
 module.exports = pool
